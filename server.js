527ec04828311fa7898ddd587747435e58e1e013
Steph Wang <sw.steph@gmail.com>
2016-08-20 17:52:44 +0000

    whoami complete

--- server.js
+++ server.js
@@ -1,1 +1,16 @@
 var express = require('express');
+var app = express();
+
+app.get('/', function (req, res) {
+    var headers = req.headers;
+    var ip = headers['x-forwarded-for'];
+    var language = headers['accept-language'].split(',')[0];
+    var software = headers['user-agent'].split(/\(|\)/)[1];
+    res.send(JSON.stringify({
+        ipaddress: ip
+        , language: language
+        , software: software
+    }));
+});
+
+app.listen(8080 || process.env.PATH);
